7b8849071002b1cdff5c44f4584e941beb21a201
Bbuterfly <97316837+Bbuterfly@users.noreply.github.com>
2023-05-02 16:24:48 +0900

    Add files via upload

--- src/main/java/io/cloudtype/waitlist/controller/HelloController.java
+++ src/main/java/io/cloudtype/waitlist/controller/HelloController.java
@@ -2,14 +2,16 @@ package io.cloudtype.waitlist.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import io.cloudtype.waitlist.model.Hello;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/test")
 public class HelloController {
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public Hello get() {
         return new Hello(0, "Hello, spring!!");
     }
 
-    @GetMapping("hi")
+    @GetMapping("/hi")
     public String hi() {
         return "hi";
     }
